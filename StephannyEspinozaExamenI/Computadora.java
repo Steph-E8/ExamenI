f6984afe056e12afdbf7752485c0e22501d569dd
Steph-E8 <stephanny.espinoza@ucr.ac.cr>
2021-10-08 07:25:09 -0600

    7:20

---
+++ StephannyEspinozaExamenI/Computadora.java
@@ -0,0 +1,25 @@
+
+/**
+ * Esta clase es la que le da vida a la computadora, aqui se tendra todo lo relacionado con una computadora.
+ * 
+ * @author Stephanny Espinoza
+ * @version 8/10/2021
+ */
+
+import java.util.Random;
+public class Computadora
+{
+    private int [][] matrizDeCompus;
+    private Random generador;
+
+    // inicializa los atributos
+    public Computadora(int filas, int columnas)
+    {
+        this.matrizDeCompus=new int [filas][columnas];
+        this.generador=generador;
+    }
+    
+    //public void 
+
+
+}
